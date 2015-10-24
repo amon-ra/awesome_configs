b121e3ab9aba45678aa5a00b955059e4dc6c8e8d
Aelspire <aelspire@gmail.com>
2015-10-24 23:05:05 +0200

    Add mouse binding for toggle tag in taglist

--- rc.lua
+++ rc.lua
@@ -412,6 +412,7 @@ mylayoutbox = { }
 mytaglist = { }
 mytaglist.buttons = awful.util.table.join(
     awful.button({ }, 1, awful.tag.viewonly),
+    awful.button({ }, 3, awful.tag.viewtoggle),
     awful.button({ }, 4, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
     awful.button({ }, 5, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end)
 )
